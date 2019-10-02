a90da9e0dd604e33615c5b68694a019af437aff0
Myles <mizuirosenpai@gmail.com>
2019-10-02 23:35:06 +0100

    Controller script for opening and closing door when function is called

---
+++ Treasure-Dungeon-Files/Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject OpenedDoor;
+    [SerializeField]
+    GameObject closedDoor;
+    // Update is called once per frame
+    void Start()
+    {
+        CloseDoor();
+        //OpenDoor();
+    }
+
+    public void OpenDoor(){
+        OpenedDoor.SetActive(true);
+        closedDoor.SetActive(false);
+    }
+
+    public void CloseDoor(){
+        closedDoor.SetActive(true);
+        OpenedDoor.SetActive(false);
+    }
+}
