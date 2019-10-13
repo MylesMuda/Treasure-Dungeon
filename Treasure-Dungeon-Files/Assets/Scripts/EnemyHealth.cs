cd2a42d278cb8f7ba8d82d8cad614281f7d34802
Myles <mizuirosenpai@gmail.com>
2019-10-13 14:59:19 +0100

    Added rudimentary knockback on enemies

--- Treasure-Dungeon-Files/Assets/Scripts/EnemyHealth.cs
+++ Treasure-Dungeon-Files/Assets/Scripts/EnemyHealth.cs
@@ -7,10 +7,11 @@ public class EnemyHealth : MonoBehaviour
     private int enemyHealth;
 
     [SerializeField]
-    public GameObject enemy;
-
-    [SerializeField]
     private int maxHealth;
+
+    public Rigidbody2D enemyRB;
+
+    public Animator enemyAnim;
 
     void Start(){
         enemyHealth = maxHealth;
@@ -18,17 +19,26 @@ public class EnemyHealth : MonoBehaviour
 
     public void DamageEnemy(){
         enemyHealth--;
+        //enemyAnim.SetBool("isHurt", true);
         checkEnemyHealth();
     }
 
     public void checkEnemyHealth(){
         if(enemyHealth==0){
             Debug.Log("Enemy health is Zero");
-            GameObject.Find("GameLogic").GetComponent<GameLogic>().SpawnEnemy(enemy);
+            GameObject.Find("GameLogic").GetComponent<GameLogic>().SpawnEnemy(gameObject);
         }
     }
 
     public void resetEnemyHealth(){
         enemyHealth = maxHealth;
     }
+
+    public void EnemyKnockback(Vector2 knife){     
+        Vector3 knifeDir = knife;
+        Vector3 moveDirection = gameObject.transform.position - knifeDir;
+        enemyRB.AddForce(moveDirection.normalized * 4000f);
+        //enemyAnim.SetBool("isHurt", false);
+    }
+    
 }
--- Treasure-Dungeon-Files/Assets/Scripts/knife.cs
+++ Treasure-Dungeon-Files/Assets/Scripts/knife.cs
@@ -7,15 +7,12 @@ public class knife : MonoBehaviour
     public Vector2 velocity = new Vector2(0.0f, 0.0f);
     public GameObject knight;
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
         Vector2 newPos = currentPos + velocity * Time.deltaTime;
 
         RaycastHit2D[] hits = Physics2D.LinecastAll(currentPos, newPos);
-
-        //Debug.DrawLine(currentPos, newPos, Color.red);
 
         foreach(RaycastHit2D hit in hits)
         {
@@ -25,7 +22,8 @@ public class knife : MonoBehaviour
                 if (other.CompareTag("Enemy"))
                 {
                     Destroy(gameObject);
-                    GameObject.Find("Big Demon").GetComponent<EnemyHealth>().DamageEnemy();
+                    other.GetComponent<EnemyHealth>().EnemyKnockback(newPos);
+                    other.GetComponent<EnemyHealth>().DamageEnemy();
                     break;
                 }
 
