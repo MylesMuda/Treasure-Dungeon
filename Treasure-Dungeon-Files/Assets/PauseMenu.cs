0e33c8647647b0e97fd2f1a8f94e3025d5773a5c
Myles <mizuirosenpai@gmail.com>
2019-10-14 01:47:22 +0100

    pause function implemented

---
+++ Treasure-Dungeon-Files/Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            if(isPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }        
+    }
+
+    public void Resume(){
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Pause(){
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu(){
+        Debug.Log("Loading menu!");
+    }
+
+    public void QuitGame(){
+        Debug.Log("Quitting Game!");
+    }
+}
